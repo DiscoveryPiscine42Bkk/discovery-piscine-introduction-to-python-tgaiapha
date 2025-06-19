73aad34a050237944ee24cf0a04dc4d9ee9acf99
tgaiapha <galaphakdeetipsukon@gmail.com>
2025-06-19 03:14:53 +0000

    5.3

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,4 +1,4 @@
-original_array = [2, 8, 9, 48, 8, -12, 2]
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 print(original_array)
-new_set = (x + 2 for x in original_array if x > 5)
+new_set = [x + 2 for x in original_array if x > 5]
 print(new_set)
